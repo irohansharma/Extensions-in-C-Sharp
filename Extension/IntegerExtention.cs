7e650251555f4514c3fc7e2ed081513c53bb0cbd
Rohan Sharma <rohan.sharma@nagarro.com>
2021-07-01 20:12:35 +0530

    First Commit

---
+++ Extension/IntegerExtention.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension
+{
+    public static class IntegerExtention
+    {
+        public static bool IsGreater(this int i , int x)
+        {
+
+            return i > x;
+        }
+
+        public static bool IsOdd(this int i)
+        {
+
+            return (i%2==1);
+        }
+
+        public static bool IsEven(this int i)
+        {
+
+            return (i % 2 == 0);
+        }
+
+        public static bool IsPrime(this int i)
+        {
+            for (int j = 3; j * j <= i; j++)
+            {
+                if (i % j == 0)
+                    return false;
+            }
+
+            return true;           
+        }
+
+        public static bool IsDivisibleBy(this int i, int x)
+        {
+            return (i%x==0);
+        }
+
+
+    }
+}
---
+++ Extension/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 100;
+
+            if (value.IsGreater(50))
+            {
+                Console.WriteLine("Yes Greater");
+            }
+            else
+            {
+                Console.WriteLine("NOT Greater");
+
+            }
+
+            if (value.IsOdd())
+            {
+                Console.WriteLine("Odd");
+            }
+            else
+            {
+                Console.WriteLine("NOT Odd");
+
+            }
+
+            if (value.IsEven())
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("NOT Even");
+
+            }
+
+            if (value.IsDivisibleBy(35))
+            {
+                Console.WriteLine("Yes Divisible");
+            }
+            else
+            {
+                Console.WriteLine("NOT Divisible");
+
+            }
+
+        }
+    }
+}
